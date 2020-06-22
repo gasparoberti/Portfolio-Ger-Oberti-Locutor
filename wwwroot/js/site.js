d5c6d689664399064ae04637f47cb17558180b2d
Gaspar <obertigaspar@gmail.com>
2020-06-22 20:10:18 -0300

    incorporacion de tinyMCE a lostextarea

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        tinymce.init({
+            selector: 'textarea'
+        });
+
+    });
+})(this.jQuery);
