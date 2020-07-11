714fc6fb69ca17e4e7bb161689bf01f29bcfa37d
Gaspar <obertigaspar@gmail.com>
2020-07-11 18:34:50 -0300

    organizacion, clase y bd finalizada para Tips

--- Models/Tip.cs
+++ Models/Tip.cs
@@ -43,5 +43,21 @@ namespace PortfolioCore.Models
         [NotMapped]
         [Display(Name = "Imagen")]
         public IFormFile archivoImagen { get; set; }
+
+        [StringLength(1000, ErrorMessage = "{0} debe tener una longitud de {1} caracteres como mínimo y {2} caracteres como máximo.", MinimumLength = 5)]
+        [Display(Name = "URL Video")]
+        public string video { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo requerido.")]
+        [Display(Name = "Visible")]
+        public bool visibleV { get; set; }
+
+        [StringLength(3000, ErrorMessage = "{0} debe tener una longitud de {1} caracteres como mínimo y {2} caracteres como máximo.", MinimumLength = 5)]
+        [Display(Name = "Cuerpo 2")]
+        public string contenido2 { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo requerido.")]
+        [Display(Name = "Visible")]
+        public bool visibleC2 { get; set; }
     }
 }
