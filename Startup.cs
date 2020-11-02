0120e13e266d791368f241d93a472b63850021c0
Gaspar <obertigaspar@gmail.com>
2020-11-02 02:24:03 -0300

    con la bd sqlserver anda todo. ahora falta actualizar la pagina a la misma version que tenia en la que usaba mysql

--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,8 @@ namespace PortfolioCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
